42cd895fb0b3df5d32c45bba665f08f5e2844b56
rasu007 <rashed.wd@gmail.com>
2018-07-29 00:17:04 +0600

    modifed app

--- js crush/app.js
+++ js crush/app.js
@@ -1,6 +1,32 @@
-var age = 23;
-var name = "Hasan Rashed";
-skill =["HTML", "JavaScript", "CSS", "JQuery"];
+// var age = 23;
+// var name = "Hasan Rashed";
+ skill =["HTML", "JavaScript", "CSS", "JQuery"];
 
-console.log(name);
-console.log(age);
+// console.log(name);
+// console.log(age);
+// console.log(100 == "100");
+// console.log(skill);
+// console.log(typeof skill);
+
+
+
+// var x = 18;
+
+// if (x =>18) {
+//    console.log("You can Eligible");
+   
+// }else{
+//    console.log("You can't Eligible");
+// }
+
+
+// for (let i = 0; i < skill.length; i++) {
+//    console.log(skill[i].toUpperCase());
+// }
+
+// console.log(skill.length);
+
+
+skill.forEach(function(name){
+   console.log(name + " Course");
+});
